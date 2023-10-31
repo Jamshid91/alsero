3a1e6268b141ee11312545e0311a2875d721fb27
Jamshid91 <iymon0217@gmail.com>
2023-10-31 12:39:14 +0500

    added

--- assets/js/express.js
+++ assets/js/express.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-    $('.switch-box').hide();
-    $('.switch-box:first-child').show();
-    $('.switch').click(function() {
-      let pageInfo = $(this).attr('data-radio');
-      $('.switch-box').hide();
-      $('.' + pageInfo).show();
-    })
-  });
+if($(window).innerWidth() >= 575) {
+        $(document).ready(function() {
+        $('.switch-box').hide();
+        $('.switch-box:first-child').show();
+        $('.switch').click(function() {
+          let pageInfo = $(this).attr('data-radio');
+          $('.switch-box').hide();
+          $('.' + pageInfo).show();
+        })
+      });
+}
